3621b72b802082dc34ea075adf55db06065607cc
Kym Gripal <kym.gripal@georgebrown.ca>
2022-12-05 00:12:46 -0500

    change delete stuff

--- api/routes/employees.js
+++ api/routes/employees.js
@@ -68,9 +68,9 @@ routes.put("/employees/:eid", async (req, res) => {
   });
 });
 
-routes.delete("/employees/:eid", async (req, res) => {
+routes.delete("/employees/", async (req, res) => {
   try {
-    const byeEmp = await EmployeeModel.deleteOne(req.query.eid);
+    const byeEmp = await EmployeeModel.deleteOne(req.body);
     if (!byeEmp) {
       return res.status(400).send({
         status: false,
